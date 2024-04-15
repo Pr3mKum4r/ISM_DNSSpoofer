e2dd2bc31b050399065814778f73b0df84e854ed
Pr3mKum4r <92014379+Pr3mKum4r@users.noreply.github.com>
2024-04-15 06:06:01 -0400

    Add DNS_Spoofer Code

---
+++ mitm/banner.py
@@ -0,0 +1,30 @@
+import random
+import colors
+
+# All you need is..
+
+
+class Banner:
+    def __init__(self):
+        self.sub_title = "\t\tDeveloped By Prem Kumar Singh, Aditya Vikram Singh, Adarsh Singh, Advay Banugaria\n"
+
+    def show(self):
+        print(colors.YELLOW +
+              b1 + colors.DEFAULT)
+        print(colors.GREEN + self.sub_title + colors.DEFAULT)
+
+
+b1 = Banner()
+b1 = """
+  ___ ___         ___.         .__    .___   _________                     ____
+ /   |   \ ___.__.\_ |_________|__| __| _/  /   _____/_____   ____   _____/ ____\___________  ______
+/    ~    <   |  | | __ \_  __ \  |/ __ |   \_____  \\____ \ /  _ \ /  _ \   __\/ __ \_  __ \/  ___/
+\    Y    /\___  | | \_\ \  | \/  / /_/ |   /        \  |_> >  <_> |  <_> )  | \  ___/|  | \/\___ \ 
+ \___|_  / / ____| |___  /__|  |__\____ |  /_______  /   __/ \____/ \____/|__|  \___  >__|  /____  >
+       \/  \/          \/              \/          \/|__|                           \/           \/ 
+"""
+
+
+
+
+
